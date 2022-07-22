f1c465d412eca86ab5906754314c7a19f17ac262
Joanna Bell <jjbdigital@gmail.com>
2022-07-22 14:42:37 -0500

    commit tree eaf125136210082cd9b199bfc20e1efca58af03e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+var newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = 'Joanna is the champion'
+document.body.append(newHeader);
+
